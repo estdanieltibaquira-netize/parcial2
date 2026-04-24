0497343fd67b5b86892ee3ba901b053234686a5d
estdanieltibaquira-netize <est.daniel.tibaquira@unimilitar.edu.co>
2026-04-24 12:23:22 -0500

    configuración inicial canvas y función plotPixel con los parametros dados

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+// CONFIGURACIÓN INICIAL
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 2;
+
+// FUNCIÓN PARA PINTAR PIXEL
+/**
+ * Dibuja un pixel en el canvas
+ * @param {number} x
+ * @param {number} y
+ * @param {string} color
+ */
+function plotPixel(x, y, color = "#000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
